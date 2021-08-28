e09c08fac029ba61ca2ad952358f54dedbf369ff
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-08-28 17:27:59 -0500

    Se crea objeto,js

---
+++ objetos.js
@@ -0,0 +1,17 @@
+let estudiante={
+
+    nombre:"Alex",
+    edad:32,
+    carrera:"Desarrollo Software",
+    genero:"M",
+    felefono:"3207180098",
+    estatura: 1.80,
+    hinchaDelmejor: true,
+    materias:["calculo","fisica","logica"],
+    comidas:{
+        postres:"Tiramizu",
+        fuertes:"frijoles",
+        bebidas:"limonada"
+    }
+}
+console.log(estudiante.edad+10)
